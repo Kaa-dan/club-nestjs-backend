3e023b6b37644e71d7393889c5b633ff4721d941
beingmusthafa <139311904+beingmusthafa@users.noreply.github.com>
2024-10-28 12:51:00 +0530

    ADD node shema

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { OnboardingModule } from './onboarding/onboarding.module';
+import { NodeModule } from './node/node.module';
 
 
 @Module({
   imports: [
     AuthModule,
     OnboardingModule,
+    NodeModule,
 
     
   ],
