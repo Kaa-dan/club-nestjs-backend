8ae8890632bb99aaa1df89a1969038cfc37d79f5
ashmil19 <ashmilabdulrasheed@gmail.com>
2024-11-06 10:20:33 +0530

    search finished

--- src/shared/search/search.module.ts
+++ src/shared/search/search.module.ts
@@ -1,14 +1,14 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { SearchService } from './search.service';
 import { SearchController } from './search.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { NodeSchema, Node_ } from '../entities/node.entity';
 import { Club, ClubSchema } from '../entities/club.entity';
+import { SharedModule } from '../shared.module';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: 'nodes', schema: NodeSchema }]),
-    MongooseModule.forFeature([{ name: Club.name, schema: ClubSchema }]),
+    forwardRef(() => SharedModule)
   ],
   providers: [SearchService],
   controllers: [SearchController]
