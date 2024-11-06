3669f3304eef60118c1d3c29e121228d372f1f6e
ashmil19 <ashmilabdulrasheed@gmail.com>
2024-11-06 13:29:09 +0530

    node club guard created

--- src/shared/search/search.controller.ts
+++ src/shared/search/search.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Param, Query, UseGuards } from '@nestjs/common';
 import { SearchService } from './search.service';
 
 @Controller('search')
 export class SearchController {
     constructor(private readonly searchService: SearchService) { }
 
-    @Get()
-    async search(@Query('term') term: string, @Query('tag') tag?: string) {
+    @Get(':id')
+    async search(@Query('term') term: string, @Query('tag') tag?: string, @Param('id') id?: string) {
         return await this.searchService.search(term, tag);
     }
 }
