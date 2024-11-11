5ecd87099e12e776489102d7ce30c6651fa167b5
Muhsin-42 <muhsinofficial21@gmail.com>
2024-11-11 11:11:38 +0530

    Spin up added.

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,19 @@ import { AppModule } from './app.module';
 import { ENV } from './utils/config/env.config';
 import { printWithBorder } from './utils/text';
 import * as morgan from 'morgan';
+import { SpinUp } from 'spin-up-ping';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(morgan('dev'));
+
+  const pinger = new SpinUp({
+    url: ENV.RENDER_URL,
+    intervalMinutes: 10,
+  });
+
+  pinger.start();
+
   app.enableCors({
     // origin: ['http://localhost:3001', 'http://localhost:3000'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
